52cadbf24acf413499fc90b0eb7b25e0e8bf262b
arjuntwp <arjunkhankriyal@thewebplant.in>
2025-06-02 17:51:04 +0530

    Sticky Done

--- src/modules/u4m-anchor-nav-bar.module/module.js
+++ src/modules/u4m-anchor-nav-bar.module/module.js
@@ -1,42 +1,17 @@
-if ($('.u4m-anchor-nav.has-sticky').length) {
-	var header = $('.u4m-header');
-	var nav = $('.u4m-anchor-nav');
-	var innerNav = $('.u4m-anchor-nav .anchor-nav');
-	var finalID = $('.u4m-anchor-nav .anchor-nav .single-anchor').last().attr('href');
-	var finalSection = $(finalID)[0];
-	innerNav.css('height', nav.outerHeight());
-	$(window).on("load resize scroll", function(e) {
-		var headHeight = header.outerHeight();
-		var scroll = $(window).scrollTop();
-		var navOffset = nav.offset().top;
-		if (scroll >= navOffset - headHeight) {
-			nav.addClass('sticky');
-			innerNav.css('top', headHeight);
-		} else {
-			nav.removeClass('sticky');
-			innerNav.css('top', '0');
-		}
-		var finalOffset = $(finalSection).offset().top + $(finalSection).parent().outerHeight();
-		if (scroll >= finalOffset) {
-			nav.addClass('bottom');
-		} else {
-			nav.removeClass('bottom');
-		}
-		$('.u4m-anchor-nav .anchor-nav .single-anchor').each(function() {
-			var thisAnchor = $(this);
-			var thisId = $(this).attr('href');
-			var thisElem = $(thisId).get(0);
-			if ($(thisElem).is_on_screen()) {
-				thisAnchor.siblings().removeClass('active-anchor');
-				thisAnchor.addClass('active-anchor');
-			}
-		});
-	});
+function adjustStickySecHeight() {
+  var navHeight = $(".u4m-anchor-nav-bar").outerHeight();
+  $(".sticky-offset").css("min-height", navHeight),
+    $(".u4m-anchor-nav-bar").css("margin-top", -navHeight);
 }
-$.fn.is_on_screen = function() {
-	var elementTop = $(this).offset().top + ($(window).height() - 150);
-	var elementBottom = elementTop + $(this).outerHeight();
-	var viewportTop = $(window).scrollTop();
-	var viewportBottom = viewportTop + $(window).height();
-	return elementBottom > viewportTop && elementTop < viewportBottom;
-};
+$(window).on("scroll", function () {
+  var stickySecTop = $(".sticky-offset").offset().top;
+  $(window).scrollTop() >= stickySecTop
+    ? $("body").addClass("sticky")
+    : $("body").removeClass("sticky");
+}),
+  $(document).ready(function () {
+    adjustStickySecHeight();
+  }),
+  $(window).on("resize", function () {
+    adjustStickySecHeight();
+  });
